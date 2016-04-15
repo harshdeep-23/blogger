7cbf6b1e2232a2740fadb174b9044b4182af48e0
Harshdeep Singh <geniusharshu@gmail.com>
2016-04-15 23:21:13 +0530

    Add article model validations and unit tests

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @article = articles(:one)
+  end
+
+  test "should be valid" do
+    assert @article.valid?
+  end
+
+  test "should be invalid without a title" do
+    @article.title = nil
+    assert_not @article.valid?
+    @article.title = ''
+    assert_not @article.valid?
+  end
+
+  test "should be invalid without a content" do
+    @article.content = nil
+    assert_not @article.valid?
+    @article.content = ''
+    assert_not @article.valid?
+  end
+
+  test "title should not be more than 255 characters long" do
+    @article.title = 'a' * 256
+    assert_not @article.valid?
+  end
 end
