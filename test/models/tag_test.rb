78579d8f24c2d3071b4c53d1e192c5c7f2ca3d23
Harshdeep Singh <geniusharshu@gmail.com>
2016-04-16 16:11:29 +0530

    Add validations and unit tests for tag model

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @tag = tags(:one)
+  end
+
+  test "should be valid" do
+    assert @tag.valid?
+  end
+
+  test "should be invalid without a name" do
+    @tag.name = nil
+    assert_not @tag.valid?
+  end
+
+  test "name should not be more than 255 characters" do
+    @tag.name = 't' * 256
+    assert_not @tag.valid?
+  end
 end
