8671736569392cc42971e45f3efbf61b889f6888
Harshdeep Singh <geniusharshu@gmail.com>
2016-04-16 16:25:42 +0530

    Add tagging model unit tests

--- test/models/tagging_test.rb
+++ test/models/tagging_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class TaggingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @tagging = taggings(:one)
+  end
+
+  test "should be valid" do
+    assert @tagging.valid?
+  end
+
+  test "should be invalid without article_id" do
+    @tagging.article_id = nil
+    assert_not @tagging.valid?
+  end
+
+  test "should be invalid without tag_id" do
+    @tagging.tag_id = nil
+    assert_not @tagging.valid?
+  end
 end
